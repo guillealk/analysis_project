bc4e856b6edf90f855bb072f943489e818db6036
guillermo <guillealk@gmail.com>
2017-11-01 19:21:31 +0100

    check code quality

--- project.py
+++ project.py
@@ -1,35 +1,47 @@
 import psycopg2
 
-#news=> select * from articles where slug=substring('candidate-is-jerkn' from 1 for character_length('candidate-is-jerkn')-1) ;
 
 def getArticles():
     co = psycopg2.connect("dbname=news")
     cursor = co.cursor()
-    query = 'select a.title, count(*) as views from log,(select slug, title from articles) as a  where substring(path  from 10  for character_length(path)) = slug  group by a.title order by views desc limit 3;'
+    query = ('select a.title, count(*) as views '
+             'from log,(select slug, title from articles) as a '
+             'where substring(path from 10 for character_length(path)) = slug '
+             'group by a.title '
+             'order by views desc limit 3;')
     cursor.execute(query)
     POSTS = cursor.fetchall()
-    co.close()  
+    co.close()
     return POSTS
+
 
 def getAuthors():
     co = psycopg2.connect("dbname=news")
     cursor = co.cursor()
-    query = 'select a.name, count(*) as views from log,(select slug, title, au.name from articles as ar left join  authors as au on  ar.author=au.id ) as a  where substring(path  from 10  for character_length(path)) = slug  group by a.name order by views desc;'
+    query = ('select a.name, count(*) as views from log, '
+             '(select slug, title, au.name from articles as ar '
+             'left join authors as au on  ar.author=au.id ) as a '
+             'where substring(path from 10 for character_length(path)) = slug '
+             'group by a.name '
+             'order by views desc;')
     cursor.execute(query)
     POSTS = cursor.fetchall()
-    co.close()  
+    co.close()
     return POSTS
+
 
 def getErrors():
     co = psycopg2.connect("dbname=news")
     cursor = co.cursor()
     value = "'404 NOT FOUND'"
-    query = 'select date(time) as date, round(count(status)*100.0/(select count(*) from log)*100, 2) as error from log where status=%s group by date having count(status)*100.0/(select count(*) from log)*100 > 1 order by error desc;'%value
+    query = ('select date(time) as date, '
+             'round(count(status)*100.0/(select count(*) from log)*100, 2) '
+             'as error from log '
+             'where status=%s group by date '
+             'having count(status)*100.0/(select count(*) from log)*100 > 1 '
+             'order by error desc;' % value)
     cursor.execute(query)
     POSTS = cursor.fetchall()
-    co.close()  
+    co.close()
     print POSTS[0]
     return POSTS
-
-
-    
